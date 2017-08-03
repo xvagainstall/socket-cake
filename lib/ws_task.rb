0205c2238bc734b11f99182d3f478ec504f93f55
kabaakaba <t.poletaev@olymptrade.com>
2017-08-03 11:10:21 +0300

    fix

--- lib/ws_task.rb
+++ lib/ws_task.rb
@@ -2,10 +2,6 @@ class WsTask
 
     attr_accessor :name, :openmessage, :url, :task_list
 
-    def self.get_sequence
-        ObjectSpace.each_object(self)
-    end
-
     def initialize name, message, url, task_list
         self.name = name
         self.openmessage = message
