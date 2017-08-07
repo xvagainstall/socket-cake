974ced4f765c899bc427692026b3dde86332b1f0
kabaakaba <t.poletaev@olymptrade.com>
2017-08-07 15:06:22 +0300

    fix sockets creation and start

--- lib/socketcake.rb
+++ lib/socketcake.rb
@@ -1,36 +1,37 @@
 require 'websocket-eventmachine-client'
 require 'json'
-require 'net/http' 
+require 'net/http'
 class SocketCake
-    attr_accessor :task_list, :storage
-    def initialize cookie,task_list=[]
-        @cookie = cookie
-        @task_list = task_list
-        @storage = Hash.new
-    end
-    
+  attr_accessor :task_list, :storage
+
+  def initialize cookie, task_list=[]
+    @cookie = cookie
+    @task_list = task_list
+    @storage = Hash.new
+  end
+
   def roll
     EM.run do
       create_sockets @task_list
-      socks = ObjectSpace.each_object(BaseSocket).to_a
-      socks.count do |t|
-        t.start { |data| return true if @storage[t.class.to_s]=data }
+      task_list.each do |task|
+        t = self.instance_variable_get(:"@#{task.name.downcase}")
+        t.start { |data| @storage[t.class.to_s]=data; EM.stop if task == task_list.last }
       end
     end
   end
 
 
+  private
 
-    private
+  def create_sockets (task_list)
 
-    def create_sockets (task_list)
-        task_list.each do |task|
-            klass = Object.const_get(task.name)
-            if klass.superclass == BaseSocket
-                self.instance_variable_set(:"@#{task.name.downcase}",klass.new(task.task_list, task.url, task.openmessage, @cookie, task.verbose))
-            end
-        end
+    task_list.each do |task|
+      klass = Object.const_get(task.name)
+      if klass.superclass == BaseSocket
+        self.instance_variable_set(:"@#{task.name.downcase}", klass.new(task.task_list, task.url, task.openmessage, @cookie, task.verbose))
+      end
     end
+  end
 end
 
 
